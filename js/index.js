94845c529b7db996d6dffb8e3cf469e7c073bcab
Benjamin Goza <bengoza@gmail.com>
2019-07-25 20:16:38 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,3 +2,4 @@
 //you can use this to paste in js code and have it run when index.html is loaded
 var element = document.createElement('div');
 element.innerHTML = '~Fernando STINKS!~';
+element.style.backgroundColor = '#f9f9f9';
